c68670d7937fd2bd4b9313fc3d26358e81c1c8e7
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-19 19:02:34 +0300

    strings

---
+++ 1_StringManipulation.py
@@ -0,0 +1,17 @@
+var = "It is a good evening."
+
+print(var.lower())
+
+print(var.upper())
+
+print(var.swapcase())
+
+print(var.split('a'))
+
+print(len(var))
+
+print(var.replace("evening", "morning"))
+
+print(var.startswith("I"))
+
+print(var.count('e'))
