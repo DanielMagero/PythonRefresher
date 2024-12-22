f539eac55fb519140c007ae92b7038f873b2ef7d
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-22 20:05:57 +0300

    new lines

--- 6_FileHandling.py
+++ 6_FileHandling.py
@@ -1,7 +1,7 @@
 file = open("Sample.text", 'w')
 
-file.write("Object1")
-file.write("Object2")
-file.write("Object3")
+file.write("Object1 \n")
+file.write("Object2 \n")
+file.write("Object3 \n")
 
-file.close
+file.close()
