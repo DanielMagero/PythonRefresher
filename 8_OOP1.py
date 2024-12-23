f716fcd0ca2be2301fbe8aa4fe5cf014f1ccbc3a
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-23 21:34:59 +0300

    oop

--- 8_OOP1.py
+++ 8_OOP1.py
@@ -1,2 +1,8 @@
 item1 = 'Phone'
-item1_price  = 
+item1_price  = 500000
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+
+##python treates all these as variables.
+##So we will have to repeat the thing again
