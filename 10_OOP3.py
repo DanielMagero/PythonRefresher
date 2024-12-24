5ee74f2f904dc61d2f092065794b83fb386a2083
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-24 22:53:31 +0300

    dynamic assignment

--- 10_OOP3.py
+++ 10_OOP3.py
@@ -1,20 +1,22 @@
 class Item:
-  def __init__(self): ##called automatically when the instance is created
-    print("I am created!")
+  def __init__(self, name): ##called automatically when the instance is created
+    print(f"An instance created: {name}")
+    self.name = name
 
   ##methods are functions insode the class
   def calculate_total_price(self, x, y):
     return x * y
 
-item1 = Item()
-item1.name  = "Phone"
+item1 = Item("Phone")
+
 item1.price = 500000
 item1.quantity = 5
 print(item1.calculate_total_price(item1.price, item1.quantity))
 
-item2 = Item()
-item2.name  = "Laptop"
+item2 = Item("Laptop")
 item2.price = 1500000
 item2.quantity = 2
 print(item2.calculate_total_price(item2.price, item2.quantity))
- 
+
+print(item1.name)
+print(item2.name)
