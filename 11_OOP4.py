d3ab31a40d3870ac47f4672f9ec0fe1e04a20690
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-24 23:06:41 +0300

    assertions and error messages

--- 11_OOP4.py
+++ 11_OOP4.py
@@ -1,7 +1,11 @@
 ##Giving the datatype to be received
 class Item:
   def __init__(self, name : str, price: float, quantity = 0): ##called automatically when the instance is created
-    print(f"An instance created: {name}")
+    ## Run validations to received arguments
+    assert price >= 0, f"Price {price} is not greater than or equal zero!"
+    assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+    ## Assign to self object
     self.name = name
     self.price = price
     self.quantity = quantity
@@ -10,7 +14,7 @@ class Item:
   def calculate_total_price(self):
     return self.price * self.quantity
 
-item1 = Item("Phone", 500000, 5)
+item1 = Item("Phone", 500000, -5)
 print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 15000000, 3)
