f6fd6b1b47f5b9630d5876b50820aae6fd38c261
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-24 22:57:05 +0300

    added all perimeters

--- 10_OOP3.py
+++ 10_OOP3.py
@@ -1,22 +1,23 @@
 class Item:
-  def __init__(self, name): ##called automatically when the instance is created
+  def __init__(self, name, price, quantity = 0): ##called automatically when the instance is created
     print(f"An instance created: {name}")
     self.name = name
+    self.price = price
+    self.quantity = quantity
 
   ##methods are functions insode the class
   def calculate_total_price(self, x, y):
     return x * y
 
-item1 = Item("Phone")
-
-item1.price = 500000
-item1.quantity = 5
+item1 = Item("Phone", 500000, 5)
 print(item1.calculate_total_price(item1.price, item1.quantity))
 
-item2 = Item("Laptop")
-item2.price = 1500000
-item2.quantity = 2
+item2 = Item("Laptop", 15000000, 3)
 print(item2.calculate_total_price(item2.price, item2.quantity))
 
 print(item1.name)
 print(item2.name)
+print(item1.price)
+print(item2.price)
+print(item1.quantity)
+print(item2.quantity)
