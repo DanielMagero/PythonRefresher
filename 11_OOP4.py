c71bfd10357d323765c6cb4c8271b19cd506db95
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-25 20:16:50 +0300

    dictionary of accessible attributes

--- 11_OOP4.py
+++ 11_OOP4.py
@@ -2,7 +2,7 @@
 class Item:
   ##class atttribute
   pay_rate  = 0.8 ##pay after 20% discount
-  
+
   def __init__(self, name : str, price: float, quantity = 0): ##called automatically when the instance is created
     ## Run validations to received arguments
     assert price >= 0, f"Price {price} is not greater than or equal zero!"
@@ -16,9 +16,19 @@ class Item:
   ##methods are functions insode the class
   def calculate_total_price(self):
     return self.price * self.quantity
+  
+  def 
 
 item1 = Item("Phone", 500000, 5)
 print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 15000000, 3)
 print(item2.calculate_total_price())
+
+
+print(Item.pay_rate)
+print(item1.pay_rate)
+print(item2.pay_rate)
+
+print(Item.__dict__) ## all attributes at class level
+print(item1.__dict__) ## all attributes at instance level
