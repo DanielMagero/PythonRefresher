9cdb9ad11e46605bc7f9f01dd6ad055d4ef3b8cb
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-25 20:18:00 +0300

    discount function

--- 11_OOP4.py
+++ 11_OOP4.py
@@ -17,7 +17,8 @@ class Item:
   def calculate_total_price(self):
     return self.price * self.quantity
   
-  def 
+  def apply_discount(self):
+    self.price  = self.price * Item.pay_rate
 
 item1 = Item("Phone", 500000, 5)
 print(item1.calculate_total_price())
